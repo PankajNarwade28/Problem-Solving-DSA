41eccd0b073718245fc5d9181658c75830222ab3
PankajNarwade28 <pankajnarwade258@gmail.com>
2025-07-27 12:32:23 +0530

    Added prime or not code

--- Simple/primeornot.java
+++ Simple/primeornot.java
@@ -1,7 +1,34 @@
-public static void main(String[] args){
-    System.out.println("hello world");
-}
+import java.util.Scanner;
 
-public void primeornot(){
-    main();
+public class primeornot {
+    public static int isPrime(int n) {
+        if (n <= 1) return 0;     // 0 and 1 are not prime
+        if (n == 2) return 1;     // 2 is prime
+
+        for (int div = 2; div <= Math.sqrt(n); div++) {
+            if (n % div == 0) {
+                return 0;        // Not prime
+            }
+        }
+        return 1;                // Prime
+    }
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter the Number:");
+        try (Scanner sc = new Scanner(System.in)) {
+            n = sc.nextInt();
+        }
+
+        try {
+            int s = isPrime(n);
+            if (s == 1) {
+                System.out.println("Prime Number!");
+            } else {
+                System.out.println("Not Prime Number!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
